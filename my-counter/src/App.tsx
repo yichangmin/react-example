b5a0aeff6b5f44677561a715e9b6dc1f548e7544
yichangmin <changmin.yi@myrealtrip.com>
2025-09-21 22:10:09 +0900

    Styling Setup

--- my-counter/src/App.tsx
+++ my-counter/src/App.tsx
@@ -1,5 +1,25 @@
+import styled from '@emotion/styled';
+
 function App() {
-  return <span>This is a counter</span>;
+  return (
+    <AppContainer>
+      <RedText>This is a counter</RedText>
+    </AppContainer>
+  );
 }
 
 export default App;
+
+const AppContainer = styled.div`
+  display: flex;
+  background-color: gray;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+`;
+
+const RedText = styled.span`
+  color: blue;
+  font-size: 80px;
+  font-weight: bold;
+`;
--- my-counter/src/main.tsx
+++ my-counter/src/main.tsx
@@ -1,9 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import { Global, css } from '@emotion/react';
+import emotionReset from 'emotion-reset';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Global
+      styles={css`
+        ${emotionReset}
+      `}
+    />
     <App />
   </StrictMode>
 );
